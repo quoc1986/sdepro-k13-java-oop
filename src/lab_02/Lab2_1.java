ed7d2f3ea4fbdd438eb309627a588c14aeaf08cc
ThaoHaTB <hathaok37cntt@gmail.com>
2022-12-18 15:37:39 +0700

    update lab 2

--- src/lab_02/Lab2_1.java
+++ src/lab_02/Lab2_1.java
@@ -4,25 +4,32 @@ import java.util.Scanner;
 
 public class Lab2_1 {
     public static void main(String[] args) {
+        float changeWeight;
         System.out.println("Please input your height(m):");
-        Scanner scanner=new Scanner(System.in);
-        float height=scanner.nextFloat();
+        Scanner scanner = new Scanner(System.in);
+        float height = scanner.nextFloat();
         System.out.println("Please input your weight(kg):");
-        float weight=scanner.nextFloat();
-        System.out.println("Calculate your BMI");
-        float bmi=weight/(height*2);
-        if(bmi<18.5){
-            System.out.println("You are under weight! You should increase your weight!");
+        float weight = scanner.nextFloat();
+        //Caculate BMI
+        if (height > 0 && weight > 0) {
+            System.out.println("Calculate your BMI");
+            float bmi = weight / (height * 2);
+            System.out.println("Your BMI:" + bmi);
+            float expectedMinWeight = 18.5f * height * 2 ;
+            float expectedMaxWeight = 24.9f * height * 2;
+            if (bmi < 18.5) {
+                changeWeight = expectedMinWeight - weight;
+                System.out.println("Underweight, You should increase " + changeWeight + "to be normal");
+            } else if (bmi <= 24.9) {
+                System.out.println("Normal weight");
+            } else if (bmi <= 29.9) {
+                changeWeight = weight - expectedMaxWeight;
+                System.out.println("Overweight, You should decrease " + changeWeight + "to be normal");
+            } else {
+                changeWeight = weight - expectedMaxWeight;
+                System.out.println("Obesity, You should decrease " + changeWeight + "to be normal");
+            }
         }
-        else if(bmi<29.9)
-        {
-            System.out.println("Overweight! You should decrease your weight!");
-            if(bmi<24.9)
-            System.out.println("Congratulate! Normal weight");
-        }
-        else {
-            System.out.println("Obesity! You should decrease your weight!");
-        }
-
     }
+
 }
